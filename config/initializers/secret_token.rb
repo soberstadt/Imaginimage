51edfc8a8048249ea3346ae95648499311da7bee
Spencer Oberstadt <soberstadt@gmail.com>
2011-12-13 14:24:36 -0600

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Imaginimage::Application.config.secret_token = 'e7200d3d1a54982798d49da9cead41474b4762f5d62136e9d5e5e94dbbb3955d4adc6c292fa0285862dd6f09f730f4c004d636eb68991706612ac5d5eae85e2f'
